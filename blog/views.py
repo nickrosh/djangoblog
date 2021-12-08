19ae19aa4cf58db493249a08f38306352dab3e4f
Nick Roshdieh <nickroshd@gmail.com>
2021-12-07 23:56:15 -0500

    created django project, added some functionality

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+posts = [
+    {
+        'author': 'Nick',
+        'title': 'First Post',
+        'content': 'This is the First Post',
+        'date_posted': 'August 27, 1969'
+    },
+    {
+        'author': 'John',
+        'title': 'Second Post',
+        'content': 'This is the Second Post',
+        'date_posted': 'June 2, 1999'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
