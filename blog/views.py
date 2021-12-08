41857d68b66cac64b61d6726cbd1c83e1d1cd8b0
Nick Roshdieh <nickroshd@gmail.com>
2021-12-08 01:15:50 -0500

    added first data Models and db data

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-
-posts = [
-    {
-        'author': 'Nick',
-        'title': 'First Post',
-        'content': 'This is the First Post',
-        'date_posted': 'August 27, 1969'
-    },
-    {
-        'author': 'John',
-        'title': 'Second Post',
-        'content': 'This is the Second Post',
-        'date_posted': 'June 2, 1999'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
