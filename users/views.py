3baa201d3010a5f9f6c34d342a9bd0678a41e478
Nick Roshdieh <nickroshd@gmail.com>
2021-12-14 23:37:08 -0500

    added profile updating forms

--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+
+from users.models import Profile
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register_view(request):
@@ -18,4 +21,23 @@ def register_view(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'users/profile.html', context)
