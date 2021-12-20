0b7f0ac97e72e5fb7d91116202dbb006772b2586
Nick Roshdieh <nickroshd@gmail.com>
2021-12-20 00:45:00 -0500

    Added CRUD functionality to Posts, through Class Based Views

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
     # on_delete=models.CASCADE means that if the tied Object (User) is deleted,
     # then all posts connected with that user will be deleted too
 
@@ -15,3 +17,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    # the post redirects to 
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
