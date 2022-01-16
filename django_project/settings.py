f8fcc15342efb02ae6fadeac6e4336671a8025d9
Nick Roshdieh <nickroshd@gmail.com>
2022-01-16 18:50:53 -0500

    added library to auto set postgres settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 
 import environ
+import django_on_heroku
 
 env = environ.Env()
 environ.Env.read_env()
@@ -154,3 +155,5 @@ AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_on_heroku.settings(locals())
