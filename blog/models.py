9ed9f2ea1b5f9bb65a0fc314f1bd219e539aa809
Nick Roshdieh <nickroshd@gmail.com>
2022-01-22 15:29:37 -0500

    added Task Model

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 
 
 class Post(models.Model):
+
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
@@ -25,10 +26,12 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
+
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name="comments",
+                             on_delete=models.CASCADE)
 
     class Meta:
         ordering = ('-date_posted',)
@@ -40,3 +43,13 @@ class Comment(models.Model):
         return reverse('post-detail', kwargs={'pk': self.pk})
 
     
+class Task(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, 
+                             related_name='tasks')
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    due_date = models.DateField()
+
+    def __str__(self):
+        return f'Task: {self.name} Due: {self.due_date}'
