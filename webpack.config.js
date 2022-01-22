5f93068e398d76443bcc2f59f916ae9d04985224
Nick Roshdieh <nickroshd@gmail.com>
2022-01-22 14:48:24 -0500

    Added in React and the babel-loader presets, added to about page

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
 const path = require('path');
 
+
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/preset-env", "@babel/preset-react"] }
+      },
+    ]
+  },
   entry: './assets/index.js',  // path to our input file
   output: {
     filename: 'index-bundle.js',  // output bundle file name
